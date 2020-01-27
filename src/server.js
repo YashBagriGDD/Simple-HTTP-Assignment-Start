549d3bf8725380d1ff651a914d3e99b47dbe43d0
Yash Bagri <ybagri@gmail.com>
2020-01-26 19:28:06 -0500

    Html responses added

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
-const http = require("http");
-const fs = require('fs');
+const http = require('http');
+
+const htmlHandler = require('./htmlResponses.js');
+// const textHandler = require('./textResponses.js');
+// const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-
 const onRequest = (request, response) => {
-    console.log(request.url);
-    
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(index);
-    response.end();
-}
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page':
+      htmlHandler.getPage2(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
 
 http.createServer(onRequest).listen(port);
 console.log(`Listening on port: ${port}`);
-console.log(`Index: ${index}`);
