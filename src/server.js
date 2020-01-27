d9dc565d6c9d7c5933f8ee3b167a14e3bd5f9b65
Yash Bagri <ybagri@gmail.com>
2020-01-26 19:45:00 -0500

    implemented jsonResponses and textResponses

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const htmlHandler = require('./htmlResponses.js');
-// const textHandler = require('./textResponses.js');
-// const jsonHandler = require('./jsonResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -13,8 +13,20 @@ const onRequest = (request, response) => {
     case '/':
       htmlHandler.getIndex(request, response);
       break;
-    case '/page':
+    case '/page2':
       htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
       break;
     default:
       htmlHandler.getIndex(request, response);
